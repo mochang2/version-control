67509453f37c36bc106ff7c8482489e1e8ea473e
mochang2 <qjsrodksro@gmail.com>
2020-05-29 13:06:25 +0900

    uploading first-script.py

---
+++ snake.py
@@ -0,0 +1,16 @@
+how_many_snakes=1
+snake_string="""
+Welcome to Python3!
+
+
+                ___
+              /  . .\\
+              \   ---<
+               \   /
+_______________/  /
+_________________/
+
+<3, Juno
+"""
+
+print(snake_string*how_many_snakes)
